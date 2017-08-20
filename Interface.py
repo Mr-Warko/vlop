315f9e902da0d089bc48c0a6252d2e30957d550f
Marko Vicentijevic <marko.vicentijevic@student.tut.fi>
2017-08-20 22:57:00 +0200

    adding script interface

---
+++ Interface.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 20 22:47:18 2017
+@author: Marko, Sabri
+
+Text Based user Interface
+
+"""
+
+# TODO : printa small text to present the use of the application and the
+# allowed command lines
+
+def main():
+  answer = "42"
+  
+  while answer:
+    answer == answer.lower()
+    # if answer is command 1
+      
+    # if answer command 2 ...
+      
+    if answer == "quit":
+      answer = None
+    
+    return
